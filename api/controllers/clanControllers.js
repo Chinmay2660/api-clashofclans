111ea208ed074dfa444e8d2fd72a8a8b2d041d26
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-03-03 00:03:02 +0530

    Optimized clancontrollers

--- api/controllers/clanControllers.js
+++ api/controllers/clanControllers.js
@@ -3,20 +3,28 @@ require('dotenv').config();
 
 const COC_API_KEY = process.env.COC_API_KEY;
 
-exports.getClanCurrentWarLeagueGroupInfo = async (req, res) => {
+const formatClanTag = (clanTag) => {
+    return clanTag.replace('clanTag=', '');
+};
+
+const handleRequest = async (req, res, getDataFunction) => {
     try {
         const { clanTag } = req.params;
         if (!clanTag) {
             return res.status(400).json({ message: 'Clan tag is required.' });
         }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanCurrentWarLeagueGroupData = await clanModel.getClanCurrentWarLeagueGroupData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanCurrentWarLeagueGroupData);
-        return res.status(200).json(clanCurrentWarLeagueGroupData);
+        const formattedClanTag = formatClanTag(clanTag);
+        const data = await getDataFunction(formattedClanTag, COC_API_KEY);
+        console.log("Clan Data:", data);
+        return res.status(200).json(data);
     } catch (error) {
-        console.error('Error fetching clanCurrentWarLeagueGroup data:', error);
+        console.error('Error fetching clan data:', error);
         return res.status(500).json({ message: 'Internal server error' });
     }
+};
+
+exports.getClanCurrentWarLeagueGroupInfo = async (req, res) => {
+    await handleRequest(req, res, clanModel.getClanCurrentWarLeagueGroupData);
 };
 
 exports.getClanWarLeagueIndividualWarInfo = async (req, res) => {
@@ -36,97 +44,25 @@ exports.getClanWarLeagueIndividualWarInfo = async (req, res) => {
 };
 
 exports.getClanWarInfo = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanWarData = await clanModel.getClanWarData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanWarData);
-        return res.status(200).json(clanWarData);
-    } catch (error) {
-        console.error('Error fetching clanWar data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getClanWarData);
 };
 
 exports.getSearchClanInfo = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanSearchData = await clanModel.getSearchClanData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanSearchData);
-        return res.status(200).json(clanSearchData);
-    } catch (error) {
-        console.error('Error fetching clanSearch data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getSearchClanData);
 };
 
 exports.getClanCurrentWarInfo = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanCurrentWarData = await clanModel.getClanCurrentWarData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanCurrentWarData);
-        return res.status(200).json(clanCurrentWarData);
-    } catch (error) {
-        console.error('Error fetching clanCurrentWar data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getClanCurrentWarData);
 };
 
 exports.getClanInfo = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanData = await clanModel.getClanData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanData);
-        return res.status(200).json(clanData);
-    } catch (error) {
-        console.error('Error fetching clan data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getClanData);
 };
 
 exports.getClanMembers = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanMembers = await clanModel.getClanMembersData(formattedClanTag, COC_API_KEY);
-        console.log("Clan Members Data:", clanMembers);
-        return res.status(200).json(clanMembers);
-    } catch (error) {
-        console.error('Error fetching clan members:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getClanMembersData);
 };
 
 exports.getClanCapitalRaidSeaonsInfo = async (req, res) => {
-    try {
-        const { clanTag } = req.params;
-        if (!clanTag) {
-            return res.status(400).json({ message: 'Clan tag is required.' });
-        }
-        const formattedClanTag = clanTag.replace('clanTag=', '');
-        const clanCapitalRaidSeaonsData = await clanModel.getClanCapitalRaidSeaonsData(formattedClanTag, COC_API_KEY);
-        console.log("clan Data:", clanCapitalRaidSeaonsData);
-        return res.status(200).json(clanCapitalRaidSeaonsData);
-    } catch (error) {
-        console.error('Error fetching clanCapitalRaidSeaons data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    await handleRequest(req, res, clanModel.getClanCapitalRaidSeaonsData);
 };
