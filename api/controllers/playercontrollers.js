640a105e94edf8b1c29b6fdc355535920bebf274
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-03-03 00:09:26 +0530

    feat: Add playertag as query parameter

--- api/controllers/playercontrollers.js
+++ api/controllers/playercontrollers.js
@@ -5,12 +5,11 @@ const COC_API_KEY = process.env.COC_API_KEY;
 
 exports.getPlayerInfo = async (req, res) => {
     try {
-        const { playerTag } = req.params;
+        const { playerTag } = req.query;
         if (!playerTag) {
             return res.status(400).json({ message: 'Player tag is required.' });
         }
-        const formattedPlayerTag = playerTag.replace('playerTag=', '');
-        const playerData = await playerModel.getPlayerData(formattedPlayerTag, COC_API_KEY);
+        const playerData = await playerModel.getPlayerData(playerTag, COC_API_KEY);
         console.log("Player Data:", playerData);
         return res.status(200).json(playerData);
     } catch (error) {
--- api/routes/playersRoutes.js
+++ api/routes/playersRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const playerController = require('../controllers/playercontrollers');
 
-router.get('/playerTag=:playerTag', playerController.getPlayerInfo);
+router.get('/', playerController.getPlayerInfo);
 
 module.exports = router;
