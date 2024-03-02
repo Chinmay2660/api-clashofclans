b5cdb28fe79cab5649cc825e052915b215647179
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-03-02 19:20:49 +0530

    Add routes for clan API

--- api/controllers/clanControllers.js
+++ api/controllers/clanControllers.js
@@ -1,6 +1,7 @@
 const clanModel = require('../models/clanModel');
+require('dotenv').config();
 
-const hardcodedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA1MTVkMGNiLWU1YTUtNDljNi04OGMzLTk4YzJhYjg4MGI1MiIsImlhdCI6MTcwOTM3NjEyMywic3ViIjoiZGV2ZWxvcGVyL2Y4NTE0YTFmLWE2NmItMzJmNS1mN2I5LWM3NWI3NWNjN2EyOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQzLjI0My4yMTQuMjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.FOskusHMBd3616O8AN0bRlQdLCOKm2gyDaszaiv-yT9AC16j2KITD16Q8M_iimwGfxSDmLpZXiECy66YsTEdhQ';
+const COC_API_KEY = process.env.COC_API_KEY
 
 const clanTag = '2P9C9U9Y'
 
@@ -10,7 +11,7 @@ exports.getClanInfo = async (req, res) => {
         if (!clanTag) {
             return res.status(400).json({ message: 'Clan tag is required.' });
         }
-        const clanData = await clanModel.getClanData(clanTag, hardcodedToken);
+        const clanData = await clanModel.getClanData(clanTag, COC_API_KEY);
         console.log("clan Data:", clanData);
         return res.status(200).json(clanData);
     } catch (error) {
@@ -25,7 +26,7 @@ exports.getClanMembers = async (req, res) => {
         if (!clanTag) {
             return res.status(400).json({ message: 'Clan tag is required.' });
         }
-        const clanMembers = await clanModel.getClanMembersData(clanTag, hardcodedToken);
+        const clanMembers = await clanModel.getClanMembersData(clanTag, COC_API_KEY);
         console.log("Clan Members Data:", clanMembers);
         return res.status(200).json(clanMembers);
     } catch (error) {
--- api/models/clanModel.js
+++ api/models/clanModel.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.getClanData = async (clanTag, token) => {
     try {
-        const response = await axios.get(`https://api.clashofclans.com/v1/clans/%23${clanTag}`, {
+        const response = await axios.get(`https://cocproxy.royaleapi.dev/v1/clans/%23${clanTag}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -15,7 +15,7 @@ exports.getClanData = async (clanTag, token) => {
 
 exports.getClanMembersData = async (clanTag, token) => {
     try {
-        const response = await axios.get(`https://api.clashofclans.com/v1/clans/%23${clanTag}/members`, {
+        const response = await axios.get(`https://cocproxy.royaleapi.dev/v1/clans/%23${clanTag}/members`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
